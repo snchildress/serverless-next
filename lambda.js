38aee8473be5188760152193c0e73d34a350087e
snchildress <steven.n.childress@gmail.com>
2018-12-12 07:21:44 -0600

    Use consistent syntax for exporting modules

--- lambda.js
+++ lambda.js
@@ -1,7 +1,7 @@
 const { app, server } = require('./server');
 const awsServerlessExpress = require('aws-serverless-express');
 
-exports.handler = (event, context) => {
+module.exports.handler = (event, context) => {
   app.prepare().then(() => {
     return awsServerlessExpress.proxy(
       awsServerlessExpress.createServer(server),
