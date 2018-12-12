060f002582f3cefcf3b65a5eedf03adb1a0eae41
snchildress <steven.n.childress@gmail.com>
2018-12-11 18:10:30 -0600

    Add the Header component to all pages

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import 'lucidium/styles/lucidium.css'
 
+import Header from '../components/Header'
 import Avatar from 'lucidium/Avatar'
 import Link from 'next-serverless/link'
 
 export default () => (
   <div>
+    <Header />
     <Link href="/about">
       <Avatar name="About" style={{ cursor: 'pointer' }} />
     </Link>
