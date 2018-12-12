ebb2a85f33814b8b9928264bdfe34369775a8ea6
snchildress <steven.n.childress@gmail.com>
2018-12-12 12:26:39 -0600

    Make the layout styling global

--- pages/index.js
+++ pages/index.js
@@ -16,29 +16,6 @@ const Index = (props) => (
         </li>
       ))}
     </ul>
-    <style jsx>{`
-      a {
-        font-family: "Arial";
-      }
-
-      ul {
-        padding: 0;
-      }
-
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
-
-      a {
-        text-decoration: none;
-        color: blue;
-      }
-
-      a:hover {
-        opacity: 0.6;
-      }
-    `}</style>
   </Layout>
 )
 
