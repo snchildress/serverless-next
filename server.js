98f986c96b81f00d1f32ace9477ce117f96b7b27
snchildress <steven.n.childress@gmail.com>
2018-12-12 06:54:33 -0600

    Use custom-built next serverless wrapper

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const express = require('express')
 const next = require('next')
-const nextServerlessHandler = require('next-serverless/handler')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-const requestHandler = (req, res) => {
-  handle(req, res)
+const createServer = () => {
+  const server = express()
+  server.get("*", (req, res) => handle(req, res))
+  return server
+};
+
+const server = createServer()
+
+if (!process.env.LAMBDA) {
+  app.prepare().then(() => {
+    server.listen(3000, err => {
+      if (err) throw err
+      console.log(`Ready on http://localhost:3000`)
+    });
+  });
 }
 
-module.exports.handler = nextServerlessHandler(app, requestHandler, () => {
-  const server = express()
-  server.use(requestHandler)
-  server.listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
-  });
-});
+module.exports.app = app
+module.exports.server = server
